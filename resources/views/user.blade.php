54e1cc5b95accb2b5ac094cec9148c4e8fe2d531
Taufiq Hidayatulloh <hidayatullohtaufiq0@gmail.com>
2024-04-04 08:04:08 +0700

    learning about ELOQUENT

---
+++ resources/views/user.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data User</title>
+</head>
+<body>
+
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
+        </tr>
+        @foreach ($data as $d)
+        <tr>
+            <th>{{ $d->user_id }}</th>
+            <th>{{ $d->username }}</th>
+            <th>{{ $d->nama }}</th>
+            <th>{{ $d->level_id }}</th>
+        </tr>
+        @endforeach
+    </table>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +23,5 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+
+Route::get('/user', [UserController::class, 'index']);

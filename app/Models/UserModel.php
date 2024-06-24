f61a1be3c19e8f47d92d2742f42a6cafef4fda28
Taufiq Hidayatulloh <hidayatullohtaufiq0@gmail.com>
2024-06-24 15:23:22 +0700

    [JS10]Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,13 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\Authenticatable As Authenticatable;
 use Illuminate\Auth\Authenticatable as AuthenticableTrait;
 
-class UserModel extends Model implements Authenticatable
+class UserModel extends Model implements JWTSubject
 {
     use AuthenticableTrait;
     use HasFactory;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     protected $fillable = ['level_id','username','nama','password'];
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
